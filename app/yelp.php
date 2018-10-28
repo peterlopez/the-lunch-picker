fee93ff9fcb3c1306c95fe4dbf67a1377728cb76
Peter Lopez <peter@phpete.me>
2018-10-28 00:09:34 -0700

    added error handling logic

--- app/yelp.php
+++ app/yelp.php
@@ -1,5 +1,6 @@
 <?php
 header('Content-Type: application/json');
+ini_set('display_errors', 'Off');
 
 const BASE_URL = 'https://api.yelp.com/v3/businesses/search';
 
@@ -16,18 +17,26 @@ if (empty($location)) {
 
 $geolocation = $_GET['geolocation'];
 
-$requestUrl = buildRequestUrl($cuisines, $location, $geolocation);
-makeRequest($requestUrl);
-
+try {
+    $requestUrl = buildRequestUrl($cuisines, $location, $geolocation);
+    makeRequest($requestUrl);
+} catch(Exception $e) {
+    http_response_code(500);
+    die(json_encode(array('message' => $e->getMessage(), 'code' => 500)));
+}
 
 /**
  * @param string|bool $url
+ * @throws Exception
  */
 function makeRequest($url = false)
 {
-    $credentials = file_get_contents('yelp-credentials.json');
+    $credentials = file_get_contents('/etc/opt/yelp-credentials.json');
     $credentials = json_decode($credentials, true);
     $apiKey = $credentials['api_key'];
+    if (empty($apiKey)) {
+        throw new Exception("ERROR: API credentials not found");
+    }
 
     $ch = curl_init($url);
     $headers = array(
@@ -41,14 +50,13 @@ function makeRequest($url = false)
     $result = curl_exec($ch);
     echo $result;
 
-//    var_dump(curl_getinfo($ch));
-
     curl_close($ch);
 }
 
 /**
  * @param $cuisines
  * @param $location
+ * @param $geolocation
  * @return string
  */
 function buildRequestUrl($cuisines, $location, $geolocation)
