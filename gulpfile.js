d37e8c50a591d6cf227204a0afc8da70c2d0ba43
Peter Lopez <peter@phpete.me>
2018-10-28 15:19:49 -0700

    updated gulp version and gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,106 @@
 var gulp = require('gulp');
-var less = require('gulp-less');
 var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var watch = require('gulp-watch');
+const { watch, series, parallel } = require('gulp');
 
-gulp.task('default', ['js:src', 'js:lib']);
-gulp.task('watch', function() {
-    gulp.watch('./app/js/**/*.js', ['js:lib', 'js:src']);
-});
 
 /**
- * Concatenates all LESS files in css/src then compiles into css/production.css
- * and finally minifies CSS
+ * ------------------------------------
+ * Config
+ * ------------------------------------
  */
-// gulp.task('css', function() {
-//     return gulp.src('./css/src/*.less')
-//         .pipe(concat('production.less'))
-//         .pipe(gulp.dest('./css'))
-//         .pipe(less('production.css'))
-//         .pipe(gulp.dest('./css'))
-// });
+var config = {
+    paths: {
+        js: {
+            src: 'app/js',
+            lib: 'app/lib'
+        }
+    },
+    files: {
+        js: {
+            src: 'app/js/**/*.js',
+            lib: 'app/lib/**/*.js',
+            srcOutput: 'production.js',
+            libOutput: 'vendor.js'
+        }
+    }
+};
+
 
 /**
- * Concatenates all JS files
+ * ------------------------------------
+ * Build functions
+ * ------------------------------------
  */
-gulp.task('js:src', function() {
-    return gulp.src(['app/js/modules/*.js', 'app/js/*.js', '!app/js/production.js'])
-        .pipe(concat('production.js'))
-        .pipe(gulp.dest('app/js/'))
-});
+function buildDev() {
+    lintJs();
+    compileJs();
+}
+function buildProd() {
+    compileJs();
+    minifyJs();
+}
+function watchFiles() {
+    watch([
+        config.files.js.src,
+        config.files.js.lib,
+        '!'+config.paths.js.src+'/'+config.files.js.srcOutput,
+        '!'+config.paths.js.lib+'/'+config.files.js.libOutput
+    ], series(lintJs, compileJs));
+}
+
 
 /**
- * Concatenates vendor JS files in /app/lib/
+ * ------------------------------------
+ * Export tasks
+ * ------------------------------------
  */
-gulp.task('js:lib', function() {
-    return gulp.src(['app/lib/*.js', '!app/lib/vendor.js'])
-        .pipe(concat('vendor.js'))
-        .pipe(gulp.dest('app/lib/'))
-});
+if (process.env.NODE_ENV === 'production') {
+    exports.default = buildProd;
+}
+else {
+    exports.default = buildDev;
+    exports.watch = watchFiles;
+}
+
+
+/**
+ * ------------------------------------
+ * Individual build tasks
+ * ------------------------------------
+ */
+function lintJs() {
+    // TODO
+    return Promise.resolve();
+}
+function compileJs() {
+    gulp.src([config.files.js.src, '!'+config.paths.js.src+'/'+config.files.js.srcOutput])
+        .pipe(concat(config.files.js.srcOutput))
+        .pipe(gulp.dest(config.paths.js.src));
+
+    gulp.src([config.files.js.lib, '!'+config.paths.js.src+'/'+config.files.js.libOutput])
+        .pipe(concat(config.files.js.libOutput))
+        .pipe(gulp.dest(config.paths.js.lib));
+
+    return Promise.resolve();
+}
+function minifyJs() {
+    // TODO
+    return Promise.resolve();
+}
+
+/**
+ * ------------------------------------
+ * Utility functions
+ * ------------------------------------
+ */
+function log(msg) {
+    // if (typeof(msg) === 'object') {
+    //     for (var item in msg) {
+    //         if (msg.hasOwnProperty(item)) {
+    //             $.util.log($.util.colors.green(msg[item]));
+    //         }
+    //     }
+    // } else {
+    //     $.util.log($.util.colors.green(msg));
+    // }
+}
