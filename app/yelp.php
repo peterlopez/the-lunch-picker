ad88620d7969592b632aa7ae23cb45b485fa8cd2
Peter Lopez <peter@phpete.me>
2018-10-29 13:29:49 -0700

    converted PHP script code into OOP

--- app/yelp.php
+++ app/yelp.php
@@ -2,83 +2,139 @@
 header('Content-Type: application/json');
 ini_set('display_errors', 'Off');
 
-const BASE_URL = 'https://api.yelp.com/v3/businesses/search';
+$yelp = new Yelp();
+$yelp->run();
 
-// Filters
-$cuisines = $_GET['cuisines'];
-if (empty($cuisines)) {
-    $cuisines = ['restaurants'];
-}
-
-$location = $_GET['location'];
-if (empty($location)) {
-    $location = "San+Fransisco";
-}
-
-$geolocation = $_GET['geolocation'];
-
-try {
-    $requestUrl = buildRequestUrl($cuisines, $location, $geolocation);
-    makeRequest($requestUrl);
-} catch(Exception $e) {
-    http_response_code(500);
-    die(json_encode(array('message' => $e->getMessage(), 'code' => 500)));
-}
-
-/**
- * @param string|bool $url
- * @throws Exception
- */
-function makeRequest($url = false)
+class Yelp
 {
-    $credentials = file_get_contents('/etc/opt/yelp-credentials.json');
-    $credentials = json_decode($credentials, true);
-    $apiKey = $credentials['api_key'];
-    if (empty($apiKey)) {
-        throw new Exception("ERROR: API credentials not found");
+    const BASE_URL = 'https://api.yelp.com/v3/businesses/search';
+
+    // Filters
+    private $cuisines = [];
+    private $location = '';
+    private $geolocation = '';
+    private $radius = '';
+    private $price = [];
+
+    /**
+     * Makes API request based on GET parameters
+     */
+    public function run()
+    {
+        try {
+            $this->setFilters($_GET);
+            $requestUrl = $this->buildRequestUrl();
+            $this->makeRequest($requestUrl);
+        } catch(Exception $e) {
+            http_response_code(500);
+            die(json_encode(array('message' => $e->getMessage(), 'code' => 500)));
+        }
     }
 
-    $ch = curl_init($url);
-    $headers = array(
-        'Content-type: application/json',
-        'Authorization: Bearer '.$apiKey,
-    );
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_VERBOSE, true);
+    /**
+     * Set filters from GET parameters with default values
+     *
+     * @param array $getParams
+     * @return void
+     */
+    protected function setFilters($getParams)
+    {
+        $this->cuisines = $getParams['cuisines'];
+        if (empty($this->cuisines)) {
+            $this->cuisines = ['restaurants'];
+        }
 
-    $result = curl_exec($ch);
-    echo $result;
+        $this->location = $getParams['location'];
+        if (empty($this->location)) {
+            $this->location = "San+Fransisco";
+        }
 
-    curl_close($ch);
-}
+        // Don't set default value
+        $this->geolocation = $getParams['geolocation'];
 
-/**
- * @param $cuisines
- * @param $location
- * @param $geolocation
- * @return string
- */
-function buildRequestUrl($cuisines, $location, $geolocation)
-{
-    $url = BASE_URL;
-
-    $url .= '?term=lunch';
-
-    $url .= '&categories=';
-    foreach($cuisines as $cuisine) {
-        $url .= $cuisine.",";
-    }
-    $url = substr($url, 0, -1);
-
-
-    if (!empty($geolocation)) {
-        $geolocation = json_decode($geolocation, true);
-        $url .= "&latitude=".$geolocation['lat'];
-        $url .= "&longitude=".$geolocation['lng'];
-    } else {
-        $url .= "&location=".urlencode($location);
+        $this->radius = $getParams['radius'];
+        if (empty($this->radius)) {
+            $this->radius = '';
+        }
     }
 
-    return $url;
+
+    /**
+     * @param string|bool $url
+     * @throws Exception
+     */
+    protected function makeRequest($url = false)
+    {
+        if (empty($url)) {
+            throw new Exception('ERROR: request URL is empty');
+        }
+
+        $apiKey = $this->getYelpApiToken();
+
+        $ch = curl_init($url);
+        $headers = array(
+            'Content-type: application/json',
+            'Authorization: Bearer '.$apiKey,
+        );
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        //curl_setopt($ch, CURLOPT_VERBOSE, true);
+
+        $result = curl_exec($ch);
+        echo $result;
+
+        curl_close($ch);
+    }
+
+    /**
+     * @return string
+     * @throws Exception
+     */
+    protected function getYelpApiToken()
+    {
+        $credentials = file_get_contents('/etc/opt/yelp-credentials.json');
+        $credentials = json_decode($credentials, true);
+        $apiKey = $credentials['api_key'];
+        if (empty($apiKey)) {
+            throw new Exception("ERROR: API credentials not found");
+        }
+
+        return $apiKey;
+    }
+
+    /**
+     * @return string
+     */
+    protected function buildRequestUrl()
+    {
+        $url = self::BASE_URL;
+
+        // Term (hardcoded)
+        $url .= '?term=lunch';
+
+        // Cuisines
+        $url .= '&categories='.implode(",", $this->cuisines);
+
+        // Geolocation OR Location
+        // prioritize geolocation over location
+        if (!empty($this->geolocation)) {
+            $geolocation = json_decode($this->geolocation, true);
+            $url .= "&latitude=".$geolocation['lat'];
+            $url .= "&longitude=".$geolocation['lng'];
+        } else {
+            $url .= "&location=".urlencode($this->location);
+        }
+
+        // Radius
+        if (!empty($this->radius)) {
+            $url .= "&radius=$this->radius";
+        }
+
+        // Price
+        if (!empty($this->price)) {
+            $url .= "&price=".implode(",", $this->price);
+        }
+
+        return $url;
+    }
 }
