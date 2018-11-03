785bc421df4257784c220479a2b1d7e09d73e123
Peter Lopez <peter@phpete.me>
2018-11-03 11:04:01 -0700

    wrote class for custom spinner logic

--- app/js/modules/spinner.js
+++ app/js/modules/spinner.js
@@ -1,34 +1,116 @@
 /**
+ * vertical top-to-bottom Spinner
+ *
+ * start by measuring each item and the amount of visible items.
+ * then, append a certain number of items to the list
+ * finally, calculate CSS transform animation and
+ * in order to have a "winning" item in the middle slot
  *
  * @constructor
  */
 function Spinner()
 {
     this.$spinner = $("#spinner");
+    this.listCSSClass = 'spinner-content';
+    this.$list = function() {
+        return $("."+this.listCSSClass);
+    };
+    this.$items = function() {
+        return $("#spinner .item");
+    };
+    this.numItems = function() {
+        return this.$items().length;
+    };
+
     this.$spinBtn = $("#btn-spin");
     this.$loadingScreen = $(".loading-container");
 
-    this.rouletteOptions = {
-        speed: 8,
-        duration: 2, // in seconds
-        // stopImageNumber: 0,
+    // this.position = 0;
 
-        startCallback : function() {
-            console.log('start');
-            $("#btn-spin").prop('disabled', true);
-        },
-        slowDownCallback : function() {
-            console.log('slowDown');
-        },
-        stopCallback : function($stopEl) {
-            console.log('stop');
-            $($stopEl).addClass('winner');
-            Spinner.$spinBtn.prop('disabled', false);
-        }
+    /**
+     * index of item in original list
+     * picked to be winner
+     * (at center position when spinner stops spinning)
+     *
+     * @type {number}
+     */
+    this.winning = 0;
+
+    this.baseSpeed = .5; // in px per ms
+    this.speed = 1; // in px per ms
+    this.duration = 10000; // in ms
+    this.speedUpDuration = 1200; // in ms
+    this.slowDownDuration = 2000; // in ms
+
+    this.numItems = 0;
+    this.numVisibleItems = 0;
+    this.stopItemNumber = 0;
+
+    /**
+     * current = middle of the visible list
+     * next = 1st item outside of visible list
+     *
+     * @type {number}
+     */
+    // this.currentItem = 0;
+    // this.$currentItem = null;
+    // this.nextItem = 0;
+    // this.$nextItem = null;
+
+    this.itemHeight = 0;
+    this.containerHeight = 0;
+
+    /**
+     * @callback from Spinner.update()
+     */
+    this.init = function() {
+        Spinner.$spinner.scrollTop(0);
+
+        // calculate heights
+        this.itemHeight = this.$items().outerHeight();
+        this.containerHeight = this.$spinner.outerHeight();
+
+        // get item stats
+        this.originalListLength = this.$items().length;
+        this.numVisibleItems = Math.round(this.containerHeight / this.itemHeight);
+        // this.currentItem = Math.round(this.numVisibleItems / 2) - 1;
+        // this.$currentItem = this.$items()[this.currentItem];
+
+        // add extra items to list
+        this.distanceTotal = this.getDistanceTotal(true);
+        this.numItemsToAdd = this.getNumItemsToAdd(this.distanceTotal);
+        this.addExtraItemsToList(this.numItemsToAdd);
+        // debugger;
+
+        // pick a winner
+        this.winning = Math.floor(Math.random() * Math.floor(this.originalListLength));
+        // console.log("winning number: "+this.winning);
+
+        this.bindEventHandlers();
     };
 
-    this.init = function() {
-        this.bindEventHandlers();
+
+    /**
+     * @callback from Yelp.success()
+     */
+    this.update = function(data)
+    {
+        Spinner.$spinBtn.prop('disabled', false);
+
+        var output = "<div class='" + Spinner.listCSSClass + "'>";
+        var restaurants = data['businesses'];
+        restaurants = shuffle(restaurants);
+        for(var i=0; i < restaurants.length; i++) {
+            var restaurant = restaurants[i];
+            output += "<div class='restaurant item'><a href='" + restaurant['url'] + "' target='_blank'>";
+            output += "  <p class='restaurant-name'>" + restaurant['name'] + "</p>";
+            output += "</a></div>";
+        }
+        output += "</div>";
+
+        Spinner.$spinner.html(output);
+        Spinner.$spinner.removeClass('nonscrollable');
+        Spinner.init();
     };
 
     this.bindEventHandlers = function()
@@ -37,29 +119,164 @@ function Spinner()
     };
 
     /**
-     *
-     */
-    this.spin = function()
-    {
-        Filters.$filterApplyBtn.fadeOut();
-        Spinner.$spinBtn.prop('disabled', true);
-
-        // var numItems = $(".restaurant").length;
-        // rouletteOptions.stopImageNumber = Math.floor(Math.random() * numItems) + 1;
-
-        var roulette = $('#spinner');
-        roulette.roulette(this.rouletteOptions);
-        roulette.roulette('start');
-    };
-
-    /**
-     *
+     * Copies div containing loading content
+     * into spinner unless already there
      */
     this.displayLoadingScreen = function()
     {
+        var exsitingLoadScreen = this.$spinner.find(".loading-container");
+        if (exsitingLoadScreen.length > 0) {
+            return;
+        }
         this.$spinner.addClass("loading");
         var $loadingScreen = this.$loadingScreen.clone();
         this.$spinner.html($loadingScreen);
         $loadingScreen.fadeIn();
-    }
+    };
+
+
+    /**
+     *
+     * @param {boolean} fresh
+     * @returns {number}
+     */
+    this.getDistanceTotal = function(fresh)
+    {
+        if (this.distanceTotal !== null && !fresh) {
+            return this.distanceTotal;
+        }
+        var distanceTotal = 0;
+
+        //
+        // calculate distance for acceleration and deceleration
+        //
+
+        // ratio of time accelerating to total time
+        this.accelFactor = this.speedUpDuration / this.duration;
+        this.decelFactor = this.slowDownDuration / this.duration;
+
+        var extraDistanceAccel = this.speedUpDuration * (this.baseSpeed + (this.baseSpeed * this.accelFactor)); // compound this over duration
+        var extraDistanceDecel = this.slowDownDuration * (this.baseSpeed + (this.baseSpeed * this.decelFactor)); // compound this over duration
+        distanceTotal += extraDistanceAccel + extraDistanceDecel;
+
+        // calculate distance going constant speed (not accelerating or decelerating)
+        distanceTotal += (this.baseSpeed * (this.duration - this.slowDownDuration - this.speedUpDuration));
+        // debugger;
+        return distanceTotal;
+    };
+
+    /**
+     *
+     * @param distanceTotal
+     * @returns {number}
+     */
+    this.getNumItemsToAdd = function(distanceTotal)
+    {
+        // divide by item height
+        var numItemsToAdd = Math.round(distanceTotal / this.itemHeight);
+        return numItemsToAdd;
+    };
+
+    /**
+     * @param {int} numItemsToAdd
+     */
+    this.addExtraItemsToList = function(numItemsToAdd)
+    {
+        var itemIndex = 0;
+        var $items = this.$items();
+        for(var i=0; i < numItemsToAdd; i++) {
+            if (itemIndex >= this.originalListLength) {
+                itemIndex = 0;
+            } else {
+                itemIndex++;
+            }
+            var $item = $($items[itemIndex]);
+            this.$list().append($item.clone());
+        }
+    };
+
+    /**
+     * @callback from spin button click event
+     */
+    this.spin = function()
+    {
+        // debugger;
+
+        Filters.$filterApplyBtn.fadeOut();
+        Spinner.$spinBtn.prop('disabled', true);
+        Spinner.$spinner.addClass('nonscrollable');
+
+        var listLength = Spinner.$items().length;
+
+        // calculate stopping point of acceleration
+        // var accelFinishPoint = Spinner.speedUpDuration * (Spinner.baseSpeed + (Spinner.baseSpeed * Spinner.accelFactor)); // compound this over duration
+        // var accelFinishPoint = this.speedUpDuration * this.itemHeight;
+        // var constantFinishPoint = accelFinishPoint + Spinner.distanceTotal - accelFinishPoint - (Spinner.slowDownDuration * (Spinner.baseSpeed + (Spinner.baseSpeed * Spinner.accelFactor)));
+
+        // 100% - (itemHeight * (list.length - winning))
+        // var listHeight = Spinner.$list().height();
+        // var visibleAreaHeight = Spinner.$list().outerHeight();
+        // var heightOfListItemsToWinner = Spinner.itemHeight * (Spinner.numItems - Spinner.winning);
+
+        var winner = listLength - (Spinner.originalListLength - Spinner.winning);
+
+        var decelFinishPoint = ((winner * Spinner.itemHeight) - Spinner.containerHeight) + (Math.round(.5 * Spinner.numVisibleItems) * Spinner.itemHeight);
+        // var decelFinishPoint = constantFinishPoint + listHeight - visibleAreaHeight - heightOfListItemsToWinner;
+        // var decelFinishPoint = Spinner.getDistanceTotal(true) - listLength -;
+
+        //
+        // setup CSS transitions using $.transit plugin
+        //
+        // scroll up a little (wind up)
+        // TODO
+        // scroll to end
+        Spinner.$list().transition({ y: -decelFinishPoint+"px" }, Spinner.slowDownDuration, 'ease');
+        // Spinner.$list().transition({ y: -accelFinishPoint+"px" }, Spinner.speedUpDuration, 'linear');
+        // Spinner.$list().transition({ y: -constantFinishPoint+"px" }, Spinner.totalDuration - Spinner.speedUpDuration - Spinner.slowDownDuration, 'linear');
+
+        // Tag winner
+        var winnerItem = Spinner.$items()[winner];
+        $(winnerItem).addClass('winner');
+
+        // Unlock spin button to go again
+        setTimeout(function() {
+            Spinner.$spinBtn.prop('disabled', false);
+            Spinner.$spinBtn.text("spin again!");
+        }, Spinner.slowDownDuration);
+    };
 }
+
+
+/**
+ * OLD roulette plugin options
+ */
+// this.spinnerOptions = {
+//
+//     startCallback : function() {
+//         console.log('start');
+//         $("#btn-spin").prop('disabled', true);
+//     },
+//     slowDownCallback : function() {
+//         console.log('slowDown');
+//     },
+//     stopCallback : function($stopEl) {
+//         console.log('stop');
+//         $($stopEl).addClass('winner');
+//         Spinner.$spinBtn.prop('disabled', false);
+//     }
+// };
+/**
+ * OLD SPIN BUTTON EVENT HANDLER
+ */
+// this.spin = function()
+// {
+//     Filters.$filterApplyBtn.fadeOut();
+//     Spinner.$spinBtn.prop('disabled', true);
+//
+//     // var numItems = $(".restaurant").length;
+//     // rouletteOptions.stopImageNumber = Math.floor(Math.random() * numItems) + 1;
+//
+//     var roulette = $('#spinner');
+//     roulette.roulette(this.rouletteOptions);
+//     roulette.roulette('start');
+// };
