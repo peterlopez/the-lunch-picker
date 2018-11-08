51cdc06ee8616be68376a3a89e906c3823e1437a
Peter Lopez <peter@phpete.me>
2018-11-08 00:00:22 -0800

    created gulp task for minifying JS

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ const rename = require('gulp-rename');
 const sass = require('gulp-sass');
 const cleanCSS = require('gulp-clean-css');
 
+const minify = require('gulp-minify');
+
 const { watch, series, parallel } = require('gulp');
 
 /**
@@ -81,6 +83,7 @@ if (process.env.NODE_ENV === 'production') {
 else {
     exports.default = buildDev;
     exports.watch = watchFiles;
+    exports.minify = buildProd;
 }
 
 
@@ -99,7 +102,7 @@ function watchSass() {
     return watch([
         files.src,
         '!'+paths.src+'/'+files.srcOutput
-    ], series(compileSass, compileSassToCss, compileVendorCss, minifyCss));
+    ], series(compileSass, compileSassToCss, compileVendorCss));
 }
 function compileVendorCss() {
     let files = config.files.css;
@@ -146,11 +149,11 @@ function watchJs() {
 
     return watch([
         files.src,
-        '!'+paths.src+'/'+files.srcOutput
+        '!'+paths.src+'/'+files.srcOutput, // compiled
+        '!'+paths.src+'/'+files.compressed, // minified
     ], series(lintJs, compileJs, minifyJs));
 }
 function lintJs() {
-    // TODO
     return Promise.resolve();
 }
 function compileJs() {
@@ -162,8 +165,37 @@ function compileJs() {
         .pipe(gulp.dest(paths.src));
 }
 function minifyJs() {
-    // TODO
-    return Promise.resolve();
+    let files = config.files.js;
+    let paths = config.paths.js;
+    return gulp.src([paths.src + '/' + files.srcOutput])
+        .pipe(minify({
+            noSource: true,
+            ext: {
+                min:'.min.js'
+            },
+            compress: {
+                sequences: false,  // join consecutive statemets with the “comma operator”
+                properties: true,  // optimize property access: a["foo"] → a.foo
+                dead_code: true,  // discard unreachable code
+                drop_debugger: true,  // discard “debugger” statements
+                unsafe: false, // some unsafe optimizations (see below)
+                conditionals: true,  // optimize if-s and conditional expressions
+                // comparisons: true,  // optimize comparisons
+                // evaluate: true,  // evaluate constant expressions
+                // booleans: true,  // optimize boolean expressions
+                // loops: true,  // optimize loops
+                // unused: true,  // drop unused variables/functions
+                // hoist_funs: true,  // hoist function declarations
+                // hoist_vars: false, // hoist variable declarations
+                // if_return: true,  // optimize if-s followed by return/continue
+                // join_vars: true,  // join var declarations
+                // cascade: true,  // try to cascade `right` into `left` in sequences
+                // side_effects: true,  // drop side-effect-free statements
+                // warnings: true,  // warn about potentially dangerous optimizations/code
+                // global_defs: {}     // global definitions
+            }
+        }))
+        .pipe(gulp.dest(paths.src));
 }
 
 /**
